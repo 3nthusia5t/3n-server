4ff893a8647f24850c38aa2afe7fab4bd10b5a4d
3nthusia5t <3nthusiast@protonmail.com>
2024-04-29 17:06:45 +0200

    fix logs

--- backend/server/server.go
+++ backend/server/server.go
@@ -75,8 +75,8 @@ RESTART: // it's useful to be able to restart server. This is a label for goto s
 	http.HandleFunc("/GetChosenArticle", GetChosenArticleHandler(DbManager))
 
 	//Starting the server
-	l.Err(http.ListenAndServe(":http", http.HandlerFunc(redirectToHTTPS)))
-	l.Err(http.ListenAndServeTLS(":https", tlsCertPath, tlsKeyPath, nil))
+	l.Err(http.ListenAndServe(":http", http.HandlerFunc(redirectToHTTPS))).Msg("HTTP server error")
+	l.Err(http.ListenAndServeTLS(":https", tlsCertPath, tlsKeyPath, nil)).Msg("HTTPS server error")
 }
 
 func UpdateApp(articleContentPath string, databasePath string) {
