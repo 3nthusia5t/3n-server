321091609d7727d65e7b9768e67593563ff7bceb
3nthusia5t <3nthusiast@protonmail.com>
2024-07-18 17:24:12 +0200

    Config file can be passed, articles are sorted, correctly, ip is not logged

--- backend/cmd/root.go
+++ backend/cmd/root.go
@@ -46,13 +46,6 @@ to quickly create a Cobra application.`,
 	},
 }
 
-var serveCmd = &cobra.Command{
-	Use:   "serve",
-	Short: "Serve command start serving static website and basic article API.",
-	Long:  "",
-	//todo
-}
-
 var transcompileCmd = &cobra.Command{
 	Use:   "transcompile",
 	Short: "Transcompile markdown articles to HTML.",
@@ -75,8 +68,6 @@ var updateCmd = &cobra.Command{
 	},
 }
 
-var startCmd = &cobra.Command{}
-
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
@@ -87,28 +78,51 @@ func Execute() {
 }
 
 func init() {
-	cobra.OnInitialize(initConfig)
 
 	// Here you will define your flags and configuration settings.
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
-
+	cobra.OnInitialize(initConfig)
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.backend.yaml)")
 	rootCmd.Flags().StringVarP(&staticContentPath, "content", "c", "../../3n-app/build", "Provide the path to the static website")
 	rootCmd.Flags().StringVarP(&imageContentPath, "img", "", "/app/3n-articles/html/images/", "Provide the path to serve the images for articles")
 	rootCmd.PersistentFlags().StringVarP(&articleContentPath, "article", "a", "/app/3n-articles/markdown/", "Provide the path to the articles")
 	rootCmd.PersistentFlags().StringVarP(&databasePath, "database", "d", "/app/database.db", "Provide the path to a sqlite database. If the path doesn't exist, it'll create new database.")
-	transcompileCmd.Flags().StringVarP(&srcTranscompilePath, "src", "", "/app/3n-articles/markdown/", "Path to the source markdown articles.")
-	transcompileCmd.Flags().StringVarP(&dstTranscompilePath, "dst", "", "/app/3n-articles/html/", "Path to the destination for HTML articles.")
 	rootCmd.PersistentFlags().StringVarP(&tlsCertPath, "cert", "", "/app/cert.pem", "Provide the path to the TLS certificate")
 	rootCmd.PersistentFlags().StringVarP(&tlsKeyPath, "key", "", "/app/key.pem", "Provide the path to the TLS key")
+	transcompileCmd.Flags().StringVarP(&srcTranscompilePath, "src", "", "/app/3n-articles/markdown/", "Path to the source markdown articles.")
+	transcompileCmd.Flags().StringVarP(&dstTranscompilePath, "dst", "", "/app/3n-articles/html/", "Path to the destination for HTML articles.")
+
 	isDev = rootCmd.PersistentFlags().Bool("dev", false, "Debugging")
+
+	//Viper binds
+	viper.BindPFlag("content", rootCmd.Flags().Lookup("content"))
+	viper.BindPFlag("img", rootCmd.Flags().Lookup("img"))
+	viper.BindPFlag("article", rootCmd.Flags().Lookup("article"))
+	viper.BindPFlag("database", rootCmd.Flags().Lookup("database"))
+	viper.BindPFlag("cert", rootCmd.Flags().Lookup("cert"))
+	viper.BindPFlag("key", rootCmd.Flags().Lookup("key"))
+	viper.BindPFlag("dev", rootCmd.Flags().Lookup("dev"))
+	viper.BindPFlag("src", transcompileCmd.Flags().Lookup("src"))
+	viper.BindPFlag("dst", transcompileCmd.Flags().Lookup("dst"))
+
+	//Viper assings
+	staticContentPath = viper.GetString("content")
+	imageContentPath = viper.GetString("img")
+	articleContentPath = viper.GetString("article")
+	databasePath = viper.GetString("database")
+	tlsCertPath = viper.GetString("cert")
+	tlsKeyPath = viper.GetString("key")
+	*isDev = viper.GetBool("dev")
+	srcTranscompilePath = viper.GetString("src")
+	dstTranscompilePath = viper.GetString("dst")
 
 	rootCmd.AddCommand(updateCmd)
 	rootCmd.AddCommand(transcompileCmd)
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
 }
 
 // initConfig reads in config file and ENV variables if set.
@@ -117,17 +131,15 @@ func initConfig() {
 		// Use config file from the flag.
 		viper.SetConfigFile(cfgFile)
 	} else {
-		// Find home directory.
 		home, err := os.UserHomeDir()
 		cobra.CheckErr(err)
-
-		// Search config in home directory with name ".backend" (without extension).
 		viper.AddConfigPath(home)
 		viper.SetConfigType("yaml")
 		viper.SetConfigName(".backend")
 	}
 
-	viper.AutomaticEnv() // read in environment variables that match
+	// take into account enviromental variables
+	viper.AutomaticEnv()
 
 	// If a config file is found, read it in.
 	if err := viper.ReadInConfig(); err == nil {
