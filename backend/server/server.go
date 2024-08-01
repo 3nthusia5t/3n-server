a7e5c3c66aceb917661350a4d51e88b094035197
3nthusia5t <3nthusiast@protonmail.com>
2024-08-01 11:00:01 +0200

    testing

--- backend/server/server.go
+++ backend/server/server.go
@@ -58,7 +58,7 @@ func ServeApp(staticContentPath string, imagesContentPath string, externalConten
 	http.Handle("/images/", http.StripPrefix("/images/", imgServer))
 
 	http.HandleFunc("/sitemap.xml", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "sitemap.xml")
+		http.ServeFile(w, r, "/app/sitemap.xml")
 	})
 
 	//Handling API calls
